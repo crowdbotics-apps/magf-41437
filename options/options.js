efbbdcedc38c56fa8a5682d0b1c911cfc7494048
Christian Solano <christian.solano89@gmail.com>
2023-06-12 14:48:23 -0600

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "magf-41437",
-  url: "https://magf-41437.botics.co",
-  api: "https://magf-41437.botics.co/api/v1"
-}
+  "name": "magf-41437",
+  "url": "https://magf-41437.botics.co",
+  "api": "https://magf-41437.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
